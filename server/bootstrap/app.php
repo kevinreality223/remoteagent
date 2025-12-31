717f2891b92a134f4425ecf880286a23bd4f01b6
kevinreality223 <kevinreality223@proton.me>
2025-12-30 16:12:31 -0800

    Remove unsupported rate limiter hook

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,22 +16,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         // Customize exception handling here.
-    })
-    ->withRateLimiting(function () {
-        RateLimiter::for('api', function (Request $request) {
-            $clientId = optional($request->attributes->get('client'))->id ?? $request->ip();
-
-            return [
-                Limit::perMinute(60)->by($clientId.'-api'),
-            ];
-        });
-
-        RateLimiter::for('poll', function (Request $request) {
-            $clientId = optional($request->attributes->get('client'))->id ?? $request->ip();
-
-            return [
-                Limit::perMinute(120)->by($clientId.'-poll'),
-            ];
-        });
     })
     ->create();
